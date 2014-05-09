ed9ee223d5aadf171857b47a26b92cce529b56c5
gaelazzo <gaetano.lazzo@gmail.com>
2014-05-09 17:41:57 +0200

    Update EdgeCompiler.cs

--- src/edge-sql/EdgeCompiler.cs
+++ src/edge-sql/EdgeCompiler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Data;
@@ -15,10 +15,21 @@ public class EdgeCompiler
         {
             connectionString = (string)tmp;
         }
+        Func<object, Task<object>> callback = null;
+        tmp = null;
+        if (parameters.TryGetValue("callback", out tmp)) {
+            callback = (Func<object, Task<object>>)tmp;
+        }
+        int packetSize=0;
+        object defPSize = 0;
+        if (parameters.TryGetValue("packetSize", out defPSize)) {
+            packetSize = Convert.ToInt32(defPSize);
+        }
 
         return async (queryParameters) =>
         {
-            return await this.ExecuteQuery(connectionString, command, (IDictionary<string, object>)queryParameters);
+            return await this.ExecuteQuery(connectionString, command, (IDictionary<string, object>)queryParameters,
+                packetSize,callback);
         };
     }
 
@@ -33,7 +44,8 @@ public class EdgeCompiler
         }
     }
 
-    async Task<object> ExecuteQuery(string connectionString, string commandString, IDictionary<string, object> parameters)
+    async Task<object> ExecuteQuery(string connectionString, string commandString, IDictionary<string, object> parameters,
+        int packetSize, Func<object, Task<object>> callback = null)
     {
         List<object> rows = new List<object>();
 
@@ -43,49 +55,69 @@ public class EdgeCompiler
             {
                 this.AddParamaters(command, parameters);
                 await connection.OpenAsync();
-                using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.CloseConnection))
-                {
-                    object[] fieldNames = new object[reader.FieldCount];
-                    for (int i = 0; i < reader.FieldCount; i++)
-                    {
-                        fieldNames[i] = reader.GetName(i);
-                    }
-                    rows.Add(fieldNames);
+                using (SqlDataReader reader = await command.ExecuteReaderAsync(CommandBehavior.CloseConnection)) {
+                    do {
+                        Dictionary<string, object> res;
+                        object[] fieldNames = new object[reader.FieldCount];
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            fieldNames[i] = reader.GetName(i);
+                        }
+                        //rows.Add(fieldNames);
+                        res = new Dictionary<string, object>();
+                        List<object> localRows = new List<object>();
+                        res["meta"] = fieldNames;
+                        if (callback != null) {
+                            callback(res);
+                            res = new Dictionary<string, object>();
+                        }
 
-                    IDataRecord record = (IDataRecord)reader;
-                    while (await reader.ReadAsync())
-                    {
-                        object[] resultRecord = new object[record.FieldCount];
-                        record.GetValues(resultRecord);
-                        for (int i = 0; i < record.FieldCount; i++)
-                        {      
-                            Type type = record.GetFieldType(i);
-                            if (resultRecord[i] is System.DBNull)
-                            {
-                                resultRecord[i] = null;
-                            }
-                            else if (type == typeof(byte[]) || type == typeof(char[]))
-                            {
-                                resultRecord[i] = Convert.ToBase64String((byte[])resultRecord[i]);
-                            }
-                            else if (type == typeof(Guid) || type == typeof(DateTime))
-                            {
-                                resultRecord[i] = resultRecord[i].ToString();
-                            }
-                            else if (type == typeof (Int16) || type == typeof (UInt16)) {
+                        res["rows"] = localRows;
+                        IDataRecord record = (IDataRecord) reader;
+                        while (await reader.ReadAsync()) {
+                            object[] resultRecord = new object[record.FieldCount];
+                            record.GetValues(resultRecord);
+                            for (int i = 0; i < record.FieldCount; i++) {
+                                Type type = record.GetFieldType(i);
+                                if (resultRecord[i] is System.DBNull) {
+                                    resultRecord[i] = null;
+                                }
+                                else if (type == typeof (Int16) || type == typeof (UInt16)) {
                                     resultRecord[i] = Convert.ToInt32(resultRecord[i]);
-                            }
-                            else if (type == typeof(Decimal)) {
+                                }
+                                else if (type == typeof(Decimal)) {
                                     resultRecord[i] = Convert.ToDouble(resultRecord[i]);
+                                }
+                                else if (type == typeof(byte[]) || type == typeof(char[])) {
+                                    resultRecord[i] = Convert.ToBase64String((byte[]) resultRecord[i]);
+                                }
+                                else if (type == typeof (Guid)) //|| type == typeof(DateTime)
+                                {
+                                    resultRecord[i] = resultRecord[i].ToString();
+                                }
+                                else if (type == typeof (IDataReader)) {
+                                    resultRecord[i] = "<IDataReader>";
+                                }
                             }
-                            else if (type == typeof(IDataReader))
-                            {
-                                resultRecord[i] = "<IDataReader>";
+                            localRows.Add(resultRecord);
+                            if (packetSize > 0 && localRows.Count == packetSize && callback!=null) {
+                                callback(res);
+                                localRows = new List<object>();
+                                res = new Dictionary<string, object>();
+                                res["rows"] = localRows;
                             }
                         }
 
-                        rows.Add(resultRecord);
+                        if (callback != null) {
+                            if (localRows.Count > 0) {
+                                callback(res);
+                            }
+                        }
+                        else {
+                            rows.Add(res);
+                        }                        
                     }
+                    while (await reader.NextResultAsync());
+
                 }
             }
         }
